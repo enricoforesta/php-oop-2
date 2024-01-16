325539e7457d4935508bddae3d3804043f6133e3
enricoforesta <enrico.foresta97@gmail.com>
2024-01-16 14:50:46 +0100

    feat: scaffolding

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+class Prodotti
+{
+    public $nome;
+    public $prezzo;
+}
+class Categoria
+{
+}
+class Cani extends Prodotti
+{
+}
+class Gatti extends Prodotti
+{
+}
+class Cibo extends Prodotti
+{
+}
+
+class Gioco extends Prodotti
+{
+}
+
+class Cuccia extends Prodotti
+{
+}
